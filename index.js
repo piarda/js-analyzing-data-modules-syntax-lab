74937b274e6733c51a2229b82c4399235b4caa61
piarda <matt.rindini@gmail.com>
2025-07-16 11:54:52 -0400

    Create function, merge usernames, get today's date

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+require('datejs');
+// necessary to use 'datejs'
 
+function combineUsers(...args){
+  const combinedObject = { // the return object
+    users: [] // array for all merged usernames
+  }
 
+  // loop through args
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = [...combinedObject.users, ...args[i]]; // to merge arrays
+  }
+
+  const currentDate = new Date().toString('M/d/yyyy');
+    combinedObject.merge_date = currentDate;
+  // to get today's date
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
